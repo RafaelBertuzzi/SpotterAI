c8e3b0f9eff67d4ce6f844c5ae8c8cebc4e2204c
Rafael Bertuzzi <rafaelbertuzzi@MacBook-Pro-de-Rafael.local>
2025-07-21 13:56:55 -0300

    chore: add necessary packages and remove expo default code

---
+++ app.config.js
@@ -0,0 +1,39 @@
+export default {
+  expo: {
+    name: "SpotterAI",
+    slug: "SpotterAI",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "spotterai",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    plugins: [
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      BASE_URL: process.env.BASE_URL,
+    },
+  }
+};
